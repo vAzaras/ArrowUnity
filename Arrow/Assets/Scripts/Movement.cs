f1eed758042c2a4535535be2afcb5fef0bf4c311
vAzaras <adam.ludwiczak98@gmail.com>
2020-08-20 04:39:15 +0200

    Player update
    Add movment, camera, look at mouse

---
+++ Arrow/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtMouse();
+        Move();
+    }
+
+    void LookAtMouse()
+    {
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float y = Input.GetAxis("Vertical") * speed;
+
+        Vector2 mov = new Vector2(x, y);
+
+        rb2d.AddForce(mov);
+    }
+}
