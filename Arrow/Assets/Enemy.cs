5691753c98d9eccd1c2f03386c635ee5faed6688
vAzaras <adam.ludwiczak98@gmail.com>
2020-08-29 15:31:57 +0200

    xxx

--- Arrow/Assets/Enemy.cs
+++ Arrow/Assets/Enemy.cs
@@ -4,14 +4,66 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float atackRange = 3f;
+    public float followRange = 5f;
+    public float atackSpeed = 5f;
+    public Transform shootDir;
+    private float lastAtack;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
+    {
+        if(closestPlayerDist() <= atackRange)
+        {
+            Atack();
+        }
+    }
+
+    private GameObject closestPlayerObj()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        float closestDist = Mathf.Infinity;
+        GameObject closestPlayer = null;
+        foreach(GameObject player in players)
+        {
+            float dist = Vector3.Distance(transform.position, player.transform.position);
+            if(dist < closestDist)
+            {
+                closestDist = dist;
+                closestPlayer = player;
+            }
+        }
+        return closestPlayer;
+    }
+    private float closestPlayerDist()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        float closestDist = Mathf.Infinity;
+        GameObject closestPlayer = null;
+        foreach (GameObject player in players)
+        {
+            float dist = Vector3.Distance(transform.position, player.transform.position);
+            if (dist < closestDist)
+            {
+                closestDist = dist;
+                closestPlayer = player;
+            }
+        }
+        return closestDist;
+    }
+    private void Atack()
+    {
+        if(Time.time - lastAtack > 1 / atackSpeed)
+        {
+            transform.LookAt(closestPlayerObj().transform.position);
+        }
+    }
+    private void FollowPlayer()
     {
         
     }
